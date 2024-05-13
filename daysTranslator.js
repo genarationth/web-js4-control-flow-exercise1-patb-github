039d7fdabe89217a10da13588fb98f2fa76b3282
patb-github <165882561+patb-github@users.noreply.github.com>
2024-05-13 15:10:21 +0700

    Corrected behavior for non-number (NaN) inputs

--- daysTranslator.js
+++ daysTranslator.js
@@ -1,7 +1,7 @@
-const dayNumber = process.argv[2];
+const dayNumber = parseInt(process.argv[2]);
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-if (typeof dayNumber !== "number") console.log("Input must be a number");
+if (isNaN(dayNumber)) console.log("Input must be a number");
 else if (dayNumber < 0 || dayNumber > 6) console.log("Invalid day number");
 else console.log(days[dayNumber]);
